3c4d9e44d419cb34313ed1967e362007ab7c9646
Marvin Hansen <marvin.hansen@gmail.com>
2024-07-05 09:34:59 +0800

    Formatted C code in FFI example.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- examples/bzlmod/ffi/ffi/c/cc_library.cc
+++ examples/bzlmod/ffi/ffi/c/cc_library.cc
@@ -1,4 +1,5 @@
-// Copyright 2024 The Bazel examples and tutorials Authors & Contributors. // All rights reserved.
+// Copyright 2024 The Bazel examples and tutorials Authors & Contributors.
+// All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +13,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
-extern "C" int func() {
- return 123;
-}
+extern "C" int func() { return 123; }
